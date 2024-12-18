fc4d1575098b20ec9375a90d2e48a4c01ee847ff
rohit8708913 <koyeb67user@gmail.com>
2024-12-19 04:46:55 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -25,3 +25,44 @@ class SidDataBase:
         self.rqst_fsub_data = self.database['request_forcesub']
         self.rqst_fsub_Channel_data = self.database['request_forcesub_channel']
         self.store_reqLink_data = self.database['store_reqLink']
+
+    # USER MANAGEMNT
+    async def present_user(self, user_id : int):
+        found = await self.user_data.find_one({'_id': user_id})
+        return bool(found)
+
+    async def add_user(self, user_id: int):
+        await self.user_data.insert_one({'_id': user_id})
+        return
+
+    async def full_userbase(self):
+        user_docs = await self.user_data.find().to_list(length=None)
+        user_ids = []
+        for doc in user_docs:
+            user_ids.append(doc['_id'])
+
+        return user_ids
+
+    async def del_user(self, user_id: int):
+        await self.user_data.delete_one({'_id': user_id})
+        return
+
+    # CHANNEL MANAGEMENT
+    async def channel_exist(self, channel_id: int):
+        found = await self.channel_data.find_one({'_id': channel_id})
+        return bool(found)
+
+    async def add_channel(self, channel_id: int):
+        if not await self.channel_exist(channel_id):
+            await self.channel_data.insert_one({'_id': channel_id})
+            return
+
+    async def del_channel(self, channel_id: int):
+        if await self.channel_exist(channel_id):
+            await self.channel_data.delete_one({'_id': channel_id})
+            return
+
+    async def get_all_channels(self):
+        channel_docs = await self.channel_data.find().to_list(length=None)
+        channel_ids = [doc['_id'] for doc in channel_docs]
+        return channel_ids
