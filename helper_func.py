8a151cf6fdcc2c68f09b14f2be6b1ecd655c324a
rohit8708913 <koyeb67user@gmail.com>
2024-12-19 05:02:32 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -11,6 +11,46 @@ from pyrogram.errors import FloodWait
 from config import FSUB_ENABLED, FSUB_CHANNEL  # Ensure FSUB_ENABLED is imported from config
 from pyrogram.errors import UserNotParticipant, RPCError
 
+# Check user subscription in Channels in a more optimized way
+async def is_subscribed(filter, client, update):
+    Channel_ids = await db.get_all_channels()
+
+    if not Channel_ids:
+        return True
+
+    user_id = update.from_user.id
+
+    if any([user_id == OWNER_ID, await kingdb.admin_exist(user_id)]):
+        return True
+
+    # Handle the case for a single channel directly (no need for gather)
+    if len(Channel_ids) == 1:
+        return await is_userJoin(client, user_id, Channel_ids[0])
+
+    # Use asyncio gather to check multiple channels concurrently
+    tasks = [is_userJoin(client, user_id, ids) for ids in Channel_ids if ids]
+    results = await asyncio.gather(*tasks)
+
+    # If any result is False, return False; else return True
+    return all(results)
+
+
+#Chcek user subscription by specifying channel id and user id
+async def is_userJoin(client, user_id, channel_id):
+    #REQFSUB = await db.get_request_forcesub()
+    try:
+        member = await client.get_chat_member(chat_id=channel_id, user_id=user_id)
+        return member.status in {ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER}
+
+    except UserNotParticipant:
+        if await db.get_request_forcesub(): #and await privateChannel(client, channel_id):
+                return await db.reqSent_user_exist(channel_id, user_id)
+
+        return False
+
+    except Exception as e:
+        print(f"!Error on is_userJoin(): {e}")
+        return False
 
 
 async def encode(string):
