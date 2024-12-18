c3a057d2bbcc908ee3bc4926266ed86535dfcd4c
rohit8708913 <koyeb67user@gmail.com>
2024-12-19 04:52:23 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -11,17 +11,6 @@ class JoinReqs:
 
         self.user_data = self.database['users']
         self.channel_data = self.database['channels']
-        self.admins_data = self.database['admins']
-        self.banned_user_data = self.database['banned_user']
-        self.autho_user_data = self.database['autho_user']
-
-        self.auto_delete_data = self.database['auto_delete']
-        self.hide_caption_data = self.database['hide_caption']
-        self.protect_content_data = self.database['protect_content']
-        self.channel_button_data = self.database['channel_button']
-
-        self.del_timer_data = self.database['del_timer']
-        self.channel_button_link_data = self.database['channelButton_link']
 
         self.rqst_fsub_data = self.database['request_forcesub']
         self.rqst_fsub_Channel_data = self.database['request_forcesub_channel']
